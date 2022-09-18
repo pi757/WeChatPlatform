58fcd8acaf9377a4fb9e08697d1258a2fc751b32
jiaSong.pi <pi_757@163.com>
2022-09-18 14:29:38 +0800

    init

---
+++ officialAccount/src/main/java/com/snail/officialaccount/OfficialAccountApplication.java
@@ -0,0 +1,13 @@
+package com.snail.officialaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficialAccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfficialAccountApplication.class, args);
+    }
+
+}
---
+++ officialAccount/src/test/java/com/snail/officialaccount/OfficialAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snail.officialaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficialAccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
